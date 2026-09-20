b09c6ae133b6d66aa49554058312a9dc0a7285f3
Gerardo <gerardo@catalasjuin.com>
2026-09-20 17:24:45 -0300

    Refactor: se modifico la estructura de carpetas en Angular

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './componentes/header/header';
-import { Footer } from './componentes/footer/footer';
+import { Header } from './pages/dashboard/header/header';
+import { Footer } from './pages/dashboard/footer/footer';
 
 @Component({
   selector: 'app-root',
---
+++ frontend/src/app/layouts/public-layout/public-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-public-layout',
+  styleUrl: './public-layout.css',
+  templateUrl: './public-layout.html',
+})
+export class PublicLayout {}
