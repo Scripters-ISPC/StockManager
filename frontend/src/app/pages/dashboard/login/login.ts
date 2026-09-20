b41af77ab1298c8b2e577e0ed07c58ff20659fca
CheloMurua <m.chelo.m@gmail.com>
2026-09-20 18:07:19 -0300

    feat: redireccionar automaticamente al dashboard si el usuario ya esta logueado

--- frontend/src/app/pages/dashboard/login/login.ts
+++ frontend/src/app/pages/dashboard/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth';
 
@@ -9,9 +9,15 @@ import { AuthService } from '../../../core/services/auth';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  ngOnInit() {
+    if (this.authService.getRole()) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
 
   ingresarComo(rol: 'admin' | 'operario', event: Event) {
     event.preventDefault(); 
